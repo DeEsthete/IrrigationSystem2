9c89cfdbaad7eca29a759159c5ff53859a16ddfc
JekaLion <djoni-01@mail.ru>
2017-11-28 17:21:55 +0600

    Евгений Павлов сделал set/get для Hub

--- PractProject/Hub.cs
+++ PractProject/Hub.cs
@@ -14,5 +14,34 @@ namespace PractProject
         Static _stat;
         IrrigationSystem[] _vegetables;
 
+        public bool GetTurnOn()
+        {
+            return _turnON;
+        }
+
+        public void SetTurnOn(bool turnON)
+        {
+            _turnON = turnON;
+        }
+
+        public Information GetInfo()
+        {
+            return _info;
+        }
+
+        public void SetInfo(Information info)
+        {
+            _info = info;
+        }
+
+        public Static GetStat()
+        {
+            return _stat;
+        }
+
+        public void SetStat(Static stat)
+        {
+            _stat = stat;
+        }
     }
 }
