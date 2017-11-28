a444746dda84299e303efd8f46af3978b19b4425
JekaLion <djoni-01@mail.ru>
2017-11-28 17:43:08 +0600

    Сделал set/get для vegetables

--- PractProject/IrrigationSystem.cs
+++ PractProject/IrrigationSystem.cs
@@ -9,6 +9,26 @@ namespace PractProject
     public class IrrigationSystem
     {
         private Vegetables _vegetables;
-        
+        private Static _stat;
+
+        public Vegetables GetVegetables()
+        {
+            return _vegetables;
+        }
+
+        public void SetVegetables(Vegetables vegetables)
+        {
+            _vegetables = vegetables;
+        }
+
+        public Static GetStat()
+        {
+            return _stat;
+        }
+
+        public void SetStat(Static stat)
+        {
+            _stat = stat;
+        }
     }
 }
