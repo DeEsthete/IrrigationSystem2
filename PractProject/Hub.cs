fb31143f28e8088dbed35c3a29059b4eb772368c
AlyonaBalakina <alyona.balakina@mail.ru>
2017-11-28 17:55:47 +0600

    Hub cout

--- PractProject/Hub.cs
+++ PractProject/Hub.cs
@@ -43,5 +43,17 @@ namespace PractProject
         {
             _stat = stat;
         }
+        public void Menu()
+        {
+            Console.WriteLine("Система планирования автоматического орошения полей.");
+            Console.WriteLine("Выберите вид орашаемого поля:" + "1-Зерновые и хлебные злаки." + "2-Овощные" + "3-Плодовые растения");
+            Console.WriteLine("Выберите стадию развития и роста:");
+            Console.WriteLine("1-эмбриональный (семенной или половой)" + "2-ювенильный, или молодости" + "3-зрелости (половой или вегетативной)"
+                + "4-размножения (полового или вегетативного)");
+            SetStage(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Введите имеющийся запас воды в резервуарах (литрах):");
+            SetNeedWater(double.Parse(Console.ReadLine()));
+
+        }
     }
 }
